4d1b4e88da6a97c4c113e946bad95a2d66f7a4cd
Abhishek thakur <thakur11abhishek22@gmail.com>
2024-01-30 13:26:50 +0530

    modified vercel

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 require('dotenv').config()
 
 
